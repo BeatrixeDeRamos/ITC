5f33e1635989450f793ae897ede1700480d66723
BeatrixeDeRamos <beatrixederamos@gmail.com>
2026-08-19 06:28:28 +0000

    Pending changes exported from your codespace

---
+++ OE1/script.js
@@ -0,0 +1,16 @@
+function showMessage(message) {
+    switch (message) {
+        case 'About the Artist':
+            alert(
+                `Hi! my name is Vuoiru, I'm an artist and will someday become a game developer.\n\nI do both traditional and digital art. I share my artworks on insta and twitter, however, I post my arts privately on twitter.\n\nI have so many dreams that I would want to achieve and become something that I would love to be someday, that I don't know what I'd really want to be in the future lol\n\nI wish to build a community of my own someday, where I can create a safe place for people and help them with their struggles.\n\nA community that is peaceful and a place where kindred souls gather and share love with each other.`
+            );
+            break;
+        case 'About this website':
+            alert(
+                "Welcome to Vuoiru's first mini art museum website!\n\n" +
+                "In this website, I will be sharing my artworks and my characters with everyone who visited this website.\n\n" +
+                "Thank you for visiting my website and I hope you enjoy it!"
+            );
+            break;
+    }
+}
